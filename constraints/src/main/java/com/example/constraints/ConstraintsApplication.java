669d476bd1af146cbfd59f55a47e284a37b97a9b
Serhat Kayıkçı <serhat.kayikci@gmail.com>
2020-05-24 16:06:48 +0200

    WIP project skeleton

---
+++ constraints/src/main/java/com/example/constraints/ConstraintsApplication.java
@@ -0,0 +1,13 @@
+package com.example.constraints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstraintsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstraintsApplication.class, args);
+	}
+
+}
---
+++ constraints/src/test/java/com/example/constraints/ConstraintsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.constraints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstraintsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
